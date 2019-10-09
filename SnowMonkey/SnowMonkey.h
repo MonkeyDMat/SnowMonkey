d56a49f666982aa33d9374f35d4080c5234d76d7
mathieu lecoupeur <mathieulecoupeur@macbook-pro-de-mathieu.home>
2019-10-09 23:44:54 +0200

    renaming project to SnowMonkey

---
+++ SnowMonkey/SnowMonkey.h
@@ -0,0 +1,19 @@
+//
+//  SnowMonkey.h
+//  SnowMonkey
+//
+//  Created by mathieu lecoupeur on 08/07/2018.
+//  Copyright © 2018 mathieu lecoupeur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnowMonkey
+FOUNDATION_EXPORT double SnowMonkeyVersionNumber;
+
+//! Project version string for EasyList.
+FOUNDATION_EXPORT const unsigned char SnowMonkeyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnowMonkey/PublicHeader.h>
+
+
