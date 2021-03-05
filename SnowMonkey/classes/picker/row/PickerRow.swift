8c7c8f994ae10ca780e1b2eed43eb50f4bd5f04a
Mathieu Lecoupeur <mathieu.lecoupeur-ext@mousquetaires.com>
2021-03-05 08:17:02 +0100

    Adding PickerView, updating the readme and removing legacy example project

---
+++ SnowMonkey/classes/picker/row/PickerRow.swift
@@ -0,0 +1,11 @@
+//
+//  PickerRow.swift
+//  SnowMonkey
+//
+//  Created by Red10 on 04/03/2021.
+//  Copyright © 2021 mathieu lecoupeur. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ SnowMonkey/classes/picker/source/PickerSource.swift
@@ -0,0 +1,71 @@
+//
+//  PickerSource.swift
+//  SnowMonkey
+//
+//  Created by Red10 on 04/03/2021.
+//  Copyright © 2021 mathieu lecoupeur. All rights reserved.
+//
+
+import UIKit
+
+@objc public protocol PickerSourceDelegate {
+    func didSelectRow(picker: PickerView, row: Int, inComponent component: Int)
+}
+
+open class PickerSource: NSObject {
+    
+    var components: [PickerComponent] = []
+    
+    public var delegate: PickerSourceDelegate?
+    public weak var picker: PickerView?
+    
+    public init(picker: PickerView, delegate: PickerSourceDelegate? = nil) {
+        self.picker = picker
+        self.delegate = delegate
+        
+        super.init()
+        
+        self.picker?.source = self
+    }
+    
+    public func setup(components: [PickerComponent]) {
+        self.components = components
+        self.picker?.reloadAllComponents()
+    }
+    
+    public func setup(rows: [PickerRow]) {
+        self.components = [PickerComponent(layout: PickerComponentLayout.default, rows: rows)]
+        self.picker?.reloadAllComponents()
+    }
+}
+
+public struct PickerComponent {
+    var layout: PickerComponentLayout
+    var rows: [PickerRow]
+}
+
+public struct PickerRow {
+    var text: String?
+    var attributedText: NSAttributedString?
+    var view: UIView?
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+    init(attributedText: NSAttributedString) {
+        self.attributedText = attributedText
+    }
+    
+    init(view: UIView) {
+        self.view = view
+    }
+}
+
+
+public struct PickerComponentLayout {
+    var width: CGFloat
+    var height: CGFloat
+    
+    static var `default` = PickerComponentLayout(width: UIScreen.main.bounds.width, height: CGFloat(40))
+}
